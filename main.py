76eb20f2e5643e241f2e3e9041542f3c0c125fd6
SiddharthRajpal <siddharthrajpal09@gmail.com>
2021-10-26 20:18:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,12 +4,15 @@ from discord import embeds
 import os
 from keep_alive import keep_alive
 import asyncio
+from email.message import EmailMessage
+import smtplib
+
+
 
 from replit import db
 import random
 command_prefix = "."
 root = commands.Bot(command_prefix = command_prefix, help_command = None)
-
 
 mutelist = []
 bad_words = [
@@ -17,7 +20,6 @@ bad_words = [
   "kamina"
 ]
 db["y"] = False
-youtube_id = "UCoZXfoF3r-uB_uhWWPP2cJw"
 @root.event
 async def on_ready():
   print("Ready")
@@ -47,13 +49,12 @@ async def stopspam(ctx):
 
 
 @root.command(aliases = ['8ball'])
-async def _8ball(ctx, *, question="None"):
+async def _8ball(ctx, *, question= None):
 
   responses = [
   "As I see it, yes.", 
   "Ask again later.", 
   "Better not tell you now.", 
-  "Cannot predict now.", 
   "Concentrate and ask again.",
   "Don’t count on it.", 
   "It is certain.", 
@@ -61,7 +62,7 @@ async def _8ball(ctx, *, question="None"):
   "Most likely.", 
   "My reply is no.", 
   "No",
-  ""
+  "d"
   "My sources say no.",
   "Outlook not so good.", 
   "Outlook good.", 
@@ -72,30 +73,54 @@ async def _8ball(ctx, *, question="None"):
   "Yes.", 
   "Yes – definitely.", 
   "You may rely on it."]
-  await ctx.channel.send(f"```Question :- {question} \nAnswer :- {random.choice(responses)}``` ")
+  em = discord.Embed(title = "8ball Answer" , color= discord.Color.dark_gold())
+  em.add_field(name = "Question", value = question)
+  em.add_field(name = "Answer",value = random.choice(responses))
+  await ctx.channel.send(embed = em)
+    
+  
+@root.command()
+async def help(ctx):
+  hi = discord.Embed(title = "SidBBot by RandomPotato", description = "Welcome to this message. SidBot is a custom bot that I have built specifically for this server using python. I will tell you about the commands and uses of SidBot",color=discord.Color.green())
+  
+  hi.add_field(name = ".src", value = "view link for the source code")
+  hi.add_field(name = ".help", value = "Get help")
+  hi.add_field(name = ".ping",value = "Sends the current ping (latency)")
+  hi.add_field(name = ".8ball <question>",value = "Ask a Question")
+  hi.add_field(name = ".spam <message>",value = "Spam Stuff")
+  hi.add_field(name = ".stopspam",value="Stops Spamming")
+  hi.add_field(name = ".clear <amount>", value = "clears number of messages specified")
+  hi.add_field(name = ".say <stuff>", value = "says stuff")
+  hi.add_field(name = ".email <receiver> <subject> <message>", value = "Send an email to someone right within discord!!!!")
+  await ctx.channel.send(embed = hi)
+  
+@root.command()
+async def clear(ctx,amount=5):
+  await ctx.channel.purge(limit=amount)
+  await ctx.send(f"Deleted {amount} Messages :)")
 
 
 @root.command()
-async def help(ctx):
-  await ctx.channel.send(
-      """         ```           SidBot BY RandomPotato
-Welcome to this message. SidBot is a custom bot that I have built specifically for this server using python. I will tell you about the commands and uses of SidBot
+async def say(ctx, *,message):
+  em = discord.Embed(color = discord.Color.red())
+  em.add_field(name = message , value = f"- {ctx.author.name}")
+  await ctx.channel.send(embed = em)
+  
+@root.command()
+async def email(ctx, receiver, subject, *, mail):
+  try:
+    msg = EmailMessage()
+    msg.set_content(mail)
+    msg['Subject'] = subject
+    msg['From'] = "sidotpconfirmation@gmail.com"
+    msg['To'] = receiver
+    server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+    server.login("sidotpconfirmation@gmail.com", "Blocked for privacy reasons")
+    server.send_message(msg)
+    server.quit()
+  except:
+      await ctx.channel.send('Error in sending email, pls use the correct format <receiver> <subject> <message>')
 
-
-.src :- view link for the source code
-.help :- Get help
-.8ball :- Ask a Question
-.spam :- Spam Stuff
-.stopspam :- Stop Spamming
-If there are any suggestions or queries or bugs please feel free to DM me :)```""")
-
-
-
-
-
-
-
-
-keep_alive
+keep_alive()
 my_secret = os.environ['key']
 root.run(my_secret)
